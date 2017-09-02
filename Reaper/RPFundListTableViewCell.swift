a43723aba3db5ebe63a36c044e74a3e591449fd7
krayc <krayc425@gmail.com>
2017-09-02 22:49:54 +0800

    Init tabBar

---
+++ Reaper/RPFundListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RPFundListTableViewCell.swift
+//  Reaper
+//
+//  Created by 宋 奎熹 on 2017/9/2.
+//  Copyright © 2017年 宋 奎熹. All rights reserved.
+//
+
+import UIKit
+
+class RPFundListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reaper/RPTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  RPTabBarController.swift
+//  Reaper
+//
+//  Created by 宋 奎熹 on 2017/9/2.
+//  Copyright © 2017年 宋 奎熹. All rights reserved.
+//
+
+import UIKit
+
+class RPTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        let item1 = UITabBarItem(title: "首页", image: nil, selectedImage: nil)
+//        let item2 = UITabBarItem(title: "基金列表", image: nil, selectedImage: nil)
+//        let item3 = UITabBarItem(title: "个人中心", image: nil, selectedImage: nil)
+//        
+//        self.tabBar.items = [item1, item2, item3]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
